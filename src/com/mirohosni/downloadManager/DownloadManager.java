b99eb4589a7f364c3cc65ca26bc2365e839732e4
mayar <mirohosni5@gmail.com>
2026-01-29 21:55:43 +0200

    auto: MAYAR 2026-01-29T21:55:43.2266840+02:00

--- src/com/mirohosni/downloadManager/DownloadManager.java
+++ src/com/mirohosni/downloadManager/DownloadManager.java
@@ -9,7 +9,7 @@ public class DownloadManager implements Runnable{
     }
 
     public static  void main(String[] args) {
-        Thread t=new Thread 
+        Thread t=new Thread(new (new DownloadManager());)
 
 }
 
