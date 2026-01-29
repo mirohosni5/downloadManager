a0ce13afcf260135de8b18f950e930d4a21af589
mayar <mirohosni5@gmail.com>
2026-01-29 21:52:36 +0200

    auto: MAYAR 2026-01-29T21:52:36.2547187+02:00

--- src/com/mirohosni/downloadManager/DownloadManager.java
+++ src/com/mirohosni/downloadManager/DownloadManager.java
@@ -2,7 +2,7 @@ package com.mirohosni.downloadManager;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class DownloadManager implements runnable{
+public class DownloadManager implements Runnablennable{
 public static  void main(String[] args) {
         System.out.println("Hello world!");
 
