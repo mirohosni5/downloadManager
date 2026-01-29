d75e355d084b03e66f1509a8502c50406ae3fc5b
mayar <mirohosni5@gmail.com>
2026-01-29 21:45:48 +0200

    auto: MAYAR 2026-01-29T21:45:48.6193025+02:00

--- src/java/DownloadManager.java
+++ src/java/DownloadManager.java
@@ -3,7 +3,9 @@ package java;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class DownloadManager {
-public void ma
+public void main(String[] args) {
+
+}
 
 
 }
