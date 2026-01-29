797ced13f828ce2ffbda98d179a64a43f229a2ea
mayar <mirohosni5@gmail.com>
2026-01-29 21:58:58 +0200

    auto: MAYAR 2026-01-29T21:58:57.9168798+02:00

--- src/com/mirohosni/downloadManager/DownloadManager.java
+++ src/com/mirohosni/downloadManager/DownloadManager.java
@@ -5,7 +5,7 @@ package com.mirohosni.downloadManager;
 public class DownloadManager implements Runnable{
     @Override
     public void run() {
-
+for ()
     }
 
     public static  void main(String[] args) {
